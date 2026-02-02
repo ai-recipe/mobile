f9b161185f57b5d695d1c45078785da2029d8871
Musa Kundakcı <musa.kundakci@zzgtech.com>
2026-02-02 19:35:08 +0300

    feat: Implement asynchronous email/password login and registration with Redux Toolkit, integrating API calls and updating UI with loading states.

--- hooks/useInitApp.ts
+++ hooks/useInitApp.ts
@@ -1,21 +1,25 @@
+import { AppDispatch } from "@/store";
+import { initI18n } from "@/store/slices/appSlice";
 import { setIsOnboarded } from "@/store/slices/authSlice";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useCallback, useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 const useInitApp = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
   const initOnboardedStatus = useCallback(async () => {
     const onboarded = await AsyncStorage.getItem("isOnboarded");
     console.log("initOnboardedStatus", onboarded, typeof onboarded);
-    if (onboarded == "true") {
+    if (onboarded === "true") {
       dispatch(setIsOnboarded(true));
     }
   }, [dispatch]);
+
   useEffect(() => {
+    dispatch(initI18n());
     initOnboardedStatus();
-  }, [initOnboardedStatus]);
+  }, [dispatch, initOnboardedStatus]);
 };
 
 export default useInitApp;
--- store/slices/appSlice.ts
+++ store/slices/appSlice.ts
@@ -1,11 +1,25 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import i18n from "@/i18n";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import * as Localization from "expo-localization";
+
+export const initI18n = createAsyncThunk("app/initI18n", async () => {
+  const deviceLanguage = Localization.getLocales()[0].languageCode ?? "en";
+  const storedLanguage = await AsyncStorage.getItem("CURRENT_LANGUAGE");
+  const finalLanguage = storedLanguage || deviceLanguage;
+
+  await i18n.changeLanguage(finalLanguage);
+  return finalLanguage;
+});
 
 interface AppState {
   isLoading: boolean;
+  currentLanguage: string | null;
 }
 
 const initialState: AppState = {
   isLoading: false,
+  currentLanguage: null,
 };
 
 export const appSlice = createSlice({
@@ -15,9 +29,17 @@ export const appSlice = createSlice({
     setIsLoading: (state, action: PayloadAction<boolean>) => {
       state.isLoading = action.payload;
     },
+    setCurrentLanguage: (state, action: PayloadAction<string>) => {
+      state.currentLanguage = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(initI18n.fulfilled, (state, action) => {
+      state.currentLanguage = action.payload;
+    });
   },
 });
 
-export const { setIsLoading } = appSlice.actions;
+export const { setIsLoading, setCurrentLanguage } = appSlice.actions;
 
 export default appSlice.reducer;
